45f67a52074f9dd34d36ac640c0613d5a56a41a6
Elliot Sawyer <elliot@silverstripe.com>
2016-03-15 15:23:53 +1300

    ENH: Basic Auth for API requests

--- code/controller/ExternalContentAPIController.php
+++ code/controller/ExternalContentAPIController.php
@@ -1,11 +1,17 @@
 <?php 
-
-
 class ExternalContentAPIController extends Controller {
+
+	private static $realm = 'NZTA External content API';
 	
 	private static $allowed_actions = array(
 		'ExternalContent',
 	);
+
+	public function init() {
+		parent::init();
+		$realm = $this->config()->get('realm');
+		$this->member = BasicAuth::requireLogin($realm);
+	}
 	
 	public function ExternalContent(SS_HTTPRequest $request){
 		$data = ExternalContent::get();
